ad184d467e0f6c26e67930011d68ea1e0c743dd8
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-05-14 03:38:27 +0900

    Update PlaylistRoot.tsx

--- components/section/PlaylistRoot.tsx
+++ components/section/PlaylistRoot.tsx
@@ -34,6 +34,23 @@ function _PlaylistRoot() {
         >
           <Parser atom={playlistAtom} />
         </section>
+        <section
+          title="File Loader"
+          className="w-80 h-3/4 flex-col gap-4 border-green-900 border-2 p-2"
+        >
+          <h1 className="text-sm font-bold text-green-400 pb-2">Spare Keys</h1>
+          <p className="text-xs text-green-600">
+            AIzaSyDfWSttr3rL8OvRwULWnkXROgtlo3l8F-U
+            <br />
+            AIzaSyCWKq-OlOZgyEoffRIRLCtgY2-_Ab0LEGo
+            <br />
+            AIzaSyBVtr8dyY5BDPCMBrw0ix-l5BKekS5h8tU
+            <br />
+            AIzaSyBJ8gkqLXzBCtlfhQykBOuirlY9JXkV0y8
+            <br />
+            AIzaSyBKolzhFf8t25Eeu0qL_U3P62p1IO97qZ8
+          </p>
+        </section>
       </div>
     </>
   );
